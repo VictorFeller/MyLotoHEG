1d022611ea0963d05f5c1ff4e063af08e1be3a83
Victor Feller <victor.feller@he-arc.ch>
2022-11-19 09:50:23 +0100

    refactor: Utilisation d'une HashMap dans Partie Refactor collection liste de cartes de ArrayList en HashMap pour optimiser la comparaison.

--- src/main/java/ch/hegarc/ig/business/Partie.java
+++ src/main/java/ch/hegarc/ig/business/Partie.java
@@ -3,56 +3,66 @@ package ch.hegarc.ig.business;
 import ch.hegarc.ig.jackson.DataBinding.JacksonReader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Partie {
-    private static List<Carte> cartesList;
+    //Utilisation d'une HashMap pour simplifier le contrôle de la carte
+    //On évite d'itérer sur toute les cartes pour pointer sur la bonne
+    private static HashMap<String, Carte> carteHashMap;
     public static List<Integer> listeNumeros = new ArrayList<>();
 
     public void initPartie() throws IOException {
 
         //Jackson reader
-        cartesList = JacksonReader.creationCartes();
+        carteHashMap = JacksonReader.creationCartes();
 
-        //Afficher le contenu des cartes
-        for(Carte carte : cartesList){
-            if(carte.getId().equals("B5")) {
-                System.out.println();
-                for(int[] ligne : carte.getLignes()){
-                for(int i = 0; i < ligne.length; i++){
-                    System.out.print(ligne[i] + " ");
-                }
+        //Affiche le contenu des cartes
+        // Iterating HashMap through for loop
+        for (Map.Entry<String, Carte> set : carteHashMap.entrySet()) {
+            // Printing all elements of a Map
+            System.out.println(set.getKey() + " : ");
+            for(int[] ligne : set.getValue().getLignes()){
+                    for(int i = 0; i < ligne.length; i++){
+                        System.out.print(ligne[i] + " ");
+                    }
                     System.out.println();
+                }
+        }
+
+        //Affiche le contenu d'une carte spécifique
+        System.out.println("Affiche B5");
+        for(int[] ligne : carteHashMap.get("B5").getLignes()){
+            for(int i = 0; i < ligne.length; i++){
+                System.out.print(ligne[i] + " ");
             }
-            }
+            System.out.println();
         }
     }
 
     public static void controlCarte(String idCarte){
         System.out.println("Control de la carte " + idCarte);
 
-        for(Carte carte : cartesList){
-            if(carte.getId().equals(idCarte)) {
-                //Algo de comparaison
-                System.out.println();
-                for(int[] ligne : carte.getLignes()){
-                    for(int i = 0; i < ligne.length; i++){
-                        System.out.print(ligne[i] + " ");
-                    }
-                    System.out.println();
-                }
-            }
-        }
+//        for(Carte carte : cartesList){
+//            if(carte.getId().equals(idCarte)) {
+//                //Algo de comparaison
+//                System.out.println();
+//                for(int[] ligne : carte.getLignes()){
+//                    for(int i = 0; i < ligne.length; i++){
+//                        System.out.print(ligne[i] + " ");
+//                    }
+//                    System.out.println();
+//                }
+//            }
+//        }
     }
 
-    public List<Carte> getCartesList() {
-        return cartesList;
-    }
-
-    public void setCartesList(List<Carte> cartesList) {
-        this.cartesList = cartesList;
-    }
+//    public List<Carte> getCartesList() {
+//        return cartesList;
+//    }
+//
+//    public void setCartesList(List<Carte> cartesList) {
+//        this.cartesList = cartesList;
+//    }
 
     public List<Integer> getListeNumeros() {
         return listeNumeros;
--- src/main/java/ch/hegarc/ig/jackson/DataBinding/JacksonReader.java
+++ src/main/java/ch/hegarc/ig/jackson/DataBinding/JacksonReader.java
@@ -7,14 +7,16 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class JacksonReader {
 
     private static ArrayList<Carte> carteList;
+    private static HashMap<String, Carte> carteHashMap = new HashMap<>();
 
     // new methode
-    public static ArrayList<Carte> creationCartes() throws IOException {
+    public static HashMap<String, Carte> creationCartes() throws IOException {
         try {
 
             ObjectMapper om = new ObjectMapper();
@@ -25,12 +27,14 @@ public class JacksonReader {
 
             for(Carte carte : carteList) {
                 System.out.println("Création de la carte : " + carte.getId());
+                //Ajout de cette carte dans la HashMap
+                carteHashMap.put(carte.getId(), carte);
             }
 
         } catch (IOException ex) {
             throw new IOException("Erreur à la lecture du json " + ex);
         }
 
-        return carteList;
+        return carteHashMap;
     }
 }
